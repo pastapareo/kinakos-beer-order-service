2af6b240f544f51029e0df7f2a1901555f4890c9
pastapareo <kuricalonge@gmail.com>
2022-08-23 16:57:11 +0800

    Refactor

---
+++ src/main/java/com/pastapareo/beer/order/service/web/mappers/BeerOrderLineMapper.java
@@ -0,0 +1,12 @@
+package com.pastapareo.beer.order.service.web.mappers;
+
+import com.pastapareo.beer.order.service.domain.BeerOrderLine;
+import com.pastapareo.beer.order.service.web.model.BeerOrderLineDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerOrderLineMapper {
+    BeerOrderLineDto beerOrderLineToDto(BeerOrderLine line);
+
+    BeerOrderLine dtoToBeerOrderLine(BeerOrderLineDto dto);
+}
