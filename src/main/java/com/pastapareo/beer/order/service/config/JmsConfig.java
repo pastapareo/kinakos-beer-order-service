7ea4851a6316a1c5601842d9992a047e986360fe
pastapareo <kuricalonge@gmail.com>
2022-09-11 17:40:08 +0800

    [#4] Configure JMS config

---
+++ src/main/java/com/pastapareo/beer/order/service/config/JmsConfig.java
@@ -0,0 +1,16 @@
+package com.pastapareo.beer.order.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+public class JmsConfig {
+    @Bean
+    public MessageConverter jacksonJsmMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
